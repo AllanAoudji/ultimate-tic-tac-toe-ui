76481c15103f3d81d37611172496138f055ab2b4
AZIHubert <allan.aoudji@protonmail.com>
2022-06-13 21:29:26 +0200

    Renders Board's <Section />

--- __tests__/Board.test.tsx
+++ __tests__/Board.test.tsx
@@ -1,10 +1,22 @@
 import {render} from '@testing-library/react-native';
 import React from 'react';
+import {generateAssets, TileState} from 'ultimate-tic-tac-toe-algorithm';
 import Board from '../src/Board';
 
 describe('<Board />', () => {
+  let board: TileState[];
+
+  beforeEach(() => {
+    ({board} = generateAssets());
+  });
+
   it('renders a <View />', () => {
-    const {queryByTestId} = render(<Board />);
+    const {queryByTestId} = render(<Board board={board} />);
     expect(queryByTestId('board__container')).toBeTruthy();
+  });
+
+  it('renders nine <Section />', () => {
+    const {getAllByTestId} = render(<Board board={board} />);
+    expect(getAllByTestId('section__container')).toHaveLength(9);
   });
 });
--- src/Board.tsx
+++ src/Board.tsx
@@ -1,6 +1,18 @@
 import React from 'react';
 import {View} from 'react-native';
+import {getSections, TileState} from 'ultimate-tic-tac-toe-algorithm';
+import Section from './Section';
 
-const Board: React.FC = () => <View testID="board__container" />;
+interface Props {
+  board: TileState[];
+}
+
+const Board: React.FC<Props> = ({board}) => (
+  <View testID="board__container">
+    {getSections(board).map(section => (
+      <Section key={section.position} tiles={section.tiles} />
+    ))}
+  </View>
+);
 
 export default Board;
