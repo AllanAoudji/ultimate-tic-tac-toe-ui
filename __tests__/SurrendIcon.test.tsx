da6adce36e4b66af04b3b099eac542b66eb75e5d
AZIHubert <allan.aoudji@protonmail.com>
2022-06-15 17:32:11 +0200

    refactor SurrendIcon.test.tsx

--- __tests__/SurrendIcon.test.tsx
+++ __tests__/SurrendIcon.test.tsx
@@ -4,16 +4,21 @@ import {TileState} from 'ultimate-tic-tac-toe-algorithm';
 import SurrendIcon from '../src/SurrendIcon';
 
 describe('<SurrendIcon />', () => {
+  const DEFAULT_SIZE = 40,
+    PLAYER_1_COLOR = '#0012ff',
+    PLAYER_2_COLOR = '#ed1327',
+    SURREND_ICON_CONTAINER_SVG = 'surrendIcon__container--svg';
+
   it('renders a <SVG /> container', () => {
     const {queryByTestId} = render(<SurrendIcon />);
-    expect(queryByTestId('surrendIcon__container--svg')).not.toBeNull();
+    expect(queryByTestId(SURREND_ICON_CONTAINER_SVG)).not.toBeNull();
   });
 
-  it('renders a <Path /> with /fill === #0012ff', () => {
+  it(`renders a <Path /> with /fill === ${PLAYER_1_COLOR}/`, () => {
     const {getByTestId} = render(<SurrendIcon />);
     expect(
-      getByTestId('surrendIcon__container--svg').findAllByProps({
-        fill: '#0012ff',
+      getByTestId(SURREND_ICON_CONTAINER_SVG).findAllByProps({
+        fill: PLAYER_1_COLOR,
       }).length,
     ).toEqual(1);
   });
@@ -21,21 +26,26 @@ describe('<SurrendIcon />', () => {
   it('/fill/ is based on /player/', () => {
     const {getByTestId} = render(<SurrendIcon player={TileState.Player2} />);
     expect(
-      getByTestId('surrendIcon__container--svg').findAllByProps({
-        fill: '#ed1327',
+      getByTestId(SURREND_ICON_CONTAINER_SVG).findAllByProps({
+        fill: PLAYER_2_COLOR,
       }).length,
     ).toEqual(1);
   });
 
-  it('have a default /height/ and /width/ of /40/', () => {
+  it(`have a default /height/ and /width/ of /${DEFAULT_SIZE}/`, () => {
     const {getByTestId} = render(<SurrendIcon />);
-    expect(getByTestId('surrendIcon__container--svg').props.width).toBe(40);
-    expect(getByTestId('surrendIcon__container--svg').props.height).toBe(40);
+    expect(getByTestId(SURREND_ICON_CONTAINER_SVG).props.width).toBe(
+      DEFAULT_SIZE,
+    );
+    expect(getByTestId(SURREND_ICON_CONTAINER_SVG).props.height).toBe(
+      DEFAULT_SIZE,
+    );
   });
 
   it('changes /height/ and /width/ based on /size/', () => {
-    const {getByTestId} = render(<SurrendIcon size={100} />);
-    expect(getByTestId('surrendIcon__container--svg').props.width).toBe(100);
-    expect(getByTestId('surrendIcon__container--svg').props.height).toBe(100);
+    const size = 100;
+    const {getByTestId} = render(<SurrendIcon size={size} />);
+    expect(getByTestId(SURREND_ICON_CONTAINER_SVG).props.width).toBe(size);
+    expect(getByTestId(SURREND_ICON_CONTAINER_SVG).props.height).toBe(size);
   });
 });
