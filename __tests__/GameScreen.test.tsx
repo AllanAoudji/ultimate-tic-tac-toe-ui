1e09aaa5331d14e264319faca2f1423dd5dec8b9
AZIHubert <allan.aoudji@protonmail.com>
2022-06-22 17:00:20 +0200

    Trigger <QuitGameModalWrapper /> when BackHandler is pressed on <GameScreen />

--- __tests__/GameScreen.test.tsx
+++ __tests__/GameScreen.test.tsx
@@ -1,49 +1,105 @@
-import {StackActions} from '@react-navigation/native';
+import {NavigationContext, StackActions} from '@react-navigation/native';
 import {fireEvent, render, RenderAPI} from '@testing-library/react-native';
 import {mockRandomForEach} from 'jest-mock-random';
 import React from 'react';
+import {BackHandler} from 'react-native';
+import {act} from 'react-test-renderer';
 import * as ultimateTicTactToAlgorithm from 'ultimate-tic-tac-toe-algorithm';
 
 import GameScreen from '../src/GameScreen';
 
+let mockCallbacks: {[index: string]: (() => void)[]} = {};
+const helperTriggerListeners = (eventName: string) => {
+  (mockCallbacks[eventName] || []).forEach(callback => callback());
+};
+
 describe('<GameScreen />', () => {
-  const createTestProps = (props: Object) => ({
-    route: {
-      params: {
-        mode: ultimateTicTactToAlgorithm.Mode.Continue,
+  const actualNav = jest.requireActual('@react-navigation/native'),
+    createTestProps = (props: Object) => ({
+      route: {
+        params: {
+          mode: ultimateTicTactToAlgorithm.Mode.Continue,
+        },
       },
-    },
-    ...props,
-  });
-  let props: any, renderer: RenderAPI;
+      ...props,
+    }),
+    navContext = (isFocused: boolean) => ({
+      ...actualNav.navigation,
+      navigate: () => {},
+      dangerouslyGetState: () => {},
+      setOptions: () => {},
+      addListener: () => () => {},
+      isFocused: () => isFocused,
+    });
+  let props: any, renderer: (isVisible?: boolean) => RenderAPI;
 
   mockRandomForEach([0.5]);
 
   beforeEach(() => {
-    props = createTestProps({});
-    renderer = render(<GameScreen {...props} />);
+    mockCallbacks = {};
     jest.spyOn(StackActions, 'replace');
     jest.spyOn(ultimateTicTactToAlgorithm, 'play');
+    jest
+      .spyOn(BackHandler, 'addEventListener')
+      .mockImplementation(
+        (
+          eventName: 'hardwareBackPress',
+          handler: () => boolean | null | undefined,
+        ) => {
+          mockCallbacks[eventName] = mockCallbacks[eventName] || [];
+          mockCallbacks[eventName].push(handler);
+          return {
+            remove: () => {},
+          };
+        },
+      );
+    jest
+      .spyOn(BackHandler, 'removeEventListener')
+      .mockImplementation(
+        (
+          eventName: 'hardwareBackPress',
+          handler: () => boolean | null | undefined,
+        ) => {
+          const indexOf = (mockCallbacks[eventName] || []).indexOf(handler);
+          if (indexOf !== -1) {
+            mockCallbacks[eventName] = mockCallbacks[eventName].splice(
+              indexOf,
+              1,
+            );
+          }
+        },
+      );
+    props = createTestProps({});
+
+    renderer = (isVisible?: boolean) =>
+      render(
+        <NavigationContext.Provider
+          value={navContext(isVisible === undefined ? true : isVisible)}>
+          <GameScreen {...props} />
+        </NavigationContext.Provider>,
+      );
   });
 
   afterEach(() => {
     jest.spyOn(StackActions, 'replace').mockRestore();
     jest.spyOn(ultimateTicTactToAlgorithm, 'play').mockRestore();
+    jest.spyOn(BackHandler, 'addEventListener').mockRestore();
+    jest.spyOn(BackHandler, 'removeEventListener').mockRestore();
   });
 
   it('renders a <View />', () => {
-    const {queryByTestId} = renderer;
+    const {queryByTestId} = renderer();
     expect(queryByTestId('gameScreen__container')).not.toBeNull();
   });
 
   it('renders a <Game />', () => {
-    const {queryAllByTestId, queryByTestId} = renderer;
+    const {queryAllByTestId, queryByTestId} = renderer();
     expect(queryByTestId('board__container')).not.toBeNull();
     expect(queryAllByTestId('playerBoard__container')).toHaveLength(2);
   });
 
   it('calls navigation.navigate when "quit" <Pressable /> is pressed', () => {
-    const {getAllByTestId, getByText} = renderer;
+    const {getAllByTestId, getByText} = renderer();
     fireEvent.press(getAllByTestId('surrendButton__container--pressable')[0]);
     fireEvent.press(getByText('yes'));
     fireEvent.press(getByText('quit'));
@@ -51,7 +107,7 @@ describe('<GameScreen />', () => {
   });
 
   it('passes /route.params.mode/ to <Game />', () => {
-    const {getAllByTestId, getAllByText} = renderer;
+    const {getAllByTestId, getAllByText} = renderer();
     fireEvent.press(getAllByTestId('tile__container--pressable')[0]);
     fireEvent.press(getAllByText('play')[1]);
     expect(ultimateTicTactToAlgorithm.play).toHaveBeenCalledWith(
@@ -60,5 +116,40 @@ describe('<GameScreen />', () => {
         mode: ultimateTicTactToAlgorithm.Mode.Continue,
       }),
     );
+  });
+
+  it('renders <QuitGameModal /> when BackPress is pressed', () => {
+    const {queryByTestId} = renderer();
+    act(() => {
+      helperTriggerListeners('hardwareBackPress');
+    });
+    expect(BackHandler.addEventListener).toHaveBeenCalled();
+    expect(queryByTestId('quitGameModal__container')).not.toBeNull();
+  });
+
+  it('hides <QuitGameModal /> when BackPress is pressed', () => {
+    const {queryByTestId} = renderer();
+    act(() => {
+      helperTriggerListeners('hardwareBackPress');
+    });
+    act(() => {
+      helperTriggerListeners('hardwareBackPress');
+    });
+    expect(queryByTestId('quitGameModal__container')).toBeNull();
+  });
+
+  it('not calls /BackHandler.addEventListener if <GameScreen /> is not focused', () => {
+    renderer(false);
+    expect(BackHandler.addEventListener).not.toHaveBeenCalled();
+  });
+
+  it('calls /BackHandler.removeEventListener/ if component is not visible', () => {
+    const {rerender} = renderer();
+    rerender(
+      <NavigationContext.Provider value={navContext(false)}>
+        <GameScreen {...props} />
+      </NavigationContext.Provider>,
+    );
+    expect(BackHandler.removeEventListener).toHaveBeenCalled();
   });
 });
--- src/GameScreen.tsx
+++ src/GameScreen.tsx
@@ -1,18 +1,41 @@
-import {StackActions} from '@react-navigation/native';
+import {StackActions, useFocusEffect} from '@react-navigation/native';
 import React from 'react';
+import {BackHandler} from 'react-native';
 import {View} from 'react-native';
 import Game from './Game';
+import QuitGameModalWrapper from './QuitGameModalWrapper';
 
 const GameScreen: React.FC<Screen.RootStack.GameNavigationProps> = ({
   route,
 }) => {
+  const [quitGameModalVisible, setQuitGameModalVisible] =
+    React.useState<boolean>(false);
+
   const onPressQuit = React.useCallback(() => {
     StackActions.replace('Home');
   }, []);
 
+  useFocusEffect(
+    React.useCallback(() => {
+      const onBackPress = () => {
+        if (quitGameModalVisible) {
+          setQuitGameModalVisible(false);
+        } else {
+          setQuitGameModalVisible(true);
+        }
+        return true;
+      };
+      BackHandler.addEventListener('hardwareBackPress', onBackPress);
+      return () => {
+        BackHandler.removeEventListener('hardwareBackPress', onBackPress);
+      };
+    }, [quitGameModalVisible]),
+  );
+
   return (
     <View testID="gameScreen__container">
       <Game mode={route.params.mode} onPressQuit={onPressQuit} />
+      <QuitGameModalWrapper visible={quitGameModalVisible} />
     </View>
   );
 };
