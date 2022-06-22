84eda06224416944190c7087dee4fe6f62d07fa6
AZIHubert <allan.aoudji@protonmail.com>
2022-06-22 20:58:54 +0200

    Add a props.disabled to disable <WinningModal />

--- __tests__/WinningModalWrapper.test.tsx
+++ __tests__/WinningModalWrapper.test.tsx
@@ -5,38 +5,64 @@ import {TileState} from 'ultimate-tic-tac-toe-algorithm';
 import WinningModalWrapper from '../src/WinningModalWrapper';
 
 describe('<WinningModalWrapper />', () => {
+  const PLAYER_X_TEXT = 'player x',
+    NEW_GAME_TEXT = 'new game',
+    QUIT_TEXT = 'quit',
+    WINNING_MODAL_CONTAINER_TEST_ID = 'winningModal__container';
+  let onPress: jest.Mock;
+
+  beforeEach(() => {
+    onPress = jest.fn();
+  });
+
+  afterEach(() => {
+    onPress.mockRestore();
+  });
+
   it('do not renders <WinningModal /> if /winner === Empty/', () => {
     const {queryByTestId} = render(
       <WinningModalWrapper winner={TileState.Empty} />,
     );
-    expect(queryByTestId('winningModal__container')).toBeNull();
+    expect(queryByTestId(WINNING_MODAL_CONTAINER_TEST_ID)).toBeNull();
   });
 
   it('renders <WinningModal /> if /winner === Player/', () => {
     const {queryByText} = render(
       <WinningModalWrapper winner={TileState.Player1} />,
     );
-    expect(queryByText('player x')).not.toBeNull();
+    expect(queryByText(PLAYER_X_TEXT)).not.toBeNull();
   });
 
   it('passes /onPressNewGame/ to <WinningModal />', () => {
-    const onPress = jest.fn();
     const {getByText} = render(
       <WinningModalWrapper
         onPressNewGame={onPress}
         winner={TileState.Player1}
       />,
     );
-    fireEvent.press(getByText('new game'));
+    fireEvent.press(getByText(NEW_GAME_TEXT));
     expect(onPress).toHaveBeenCalled();
   });
 
   it('passes /onPressQuit/ to <WinningModal />', () => {
-    const onPress = jest.fn();
     const {getByText} = render(
       <WinningModalWrapper onPressQuit={onPress} winner={TileState.Player1} />,
     );
-    fireEvent.press(getByText('quit'));
+    fireEvent.press(getByText(QUIT_TEXT));
     expect(onPress).toHaveBeenCalled();
+  });
+
+  it('passes /disabled/ to <WinningModal />', () => {
+    const {getByText} = render(
+      <WinningModalWrapper
+        disabled={true}
+        onPressNewGame={onPress}
+        onPressQuit={onPress}
+        winner={TileState.Player1}
+      />,
+    );
+    fireEvent.press(getByText(QUIT_TEXT));
+    fireEvent.press(getByText(NEW_GAME_TEXT));
+    expect(onPress).not.toHaveBeenCalled();
   });
 });
