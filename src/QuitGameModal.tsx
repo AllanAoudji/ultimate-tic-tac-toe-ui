e98ffb9e9a30a9ebe7e2851337828667f1247e68
AZIHubert <allan.aoudji@protonmail.com>
2022-06-29 22:45:05 +0200

    Style <QuitGameModal />

--- src/QuitGameModal.tsx
+++ src/QuitGameModal.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
-import {GestureResponderEvent, Pressable} from 'react-native';
+import {
+  GestureResponderEvent,
+  Pressable,
+  StyleSheet,
+  ViewStyle,
+} from 'react-native';
 
 import Container from './Container';
+import {ThemeContext} from './Theme.context';
 import Typography from './Typography';
 
 interface Props {
@@ -14,21 +20,73 @@ const QuitGameModal: React.FC<Props> = ({
   disabled = false,
   onPressNo = () => {},
   onPressYes = () => {},
-}) => (
-  <Container testID="quitGameModal__container">
-    <Typography>quit game?</Typography>
-    <Pressable disabled={disabled} onPress={onPressYes}>
-      <Typography>yes</Typography>
-    </Pressable>
-    <Pressable disabled={disabled} onPress={onPressNo}>
-      <Typography>no</Typography>
-    </Pressable>
-    <Pressable
-      disabled={disabled}
-      onPress={onPressNo}
-      testID="quitGameModal__background--pressable"
-    />
-  </Container>
-);
+}) => {
+  const {theme} = React.useContext(ThemeContext);
+  const styles = React.useMemo(() => quitGameModalStyles(theme), [theme]);
+  return (
+    <Container
+      alignItems="center"
+      height="100%"
+      justifyContent="center"
+      position="absolute"
+      testID="quitGameModal__container"
+      width="100%">
+      <Pressable
+        disabled={disabled}
+        onPress={onPressNo}
+        style={styles.background}
+        testID="quitGameModal__background--pressable"
+      />
+      <Container alignItems="center" padding="normal" width="100%">
+        <Container
+          alignItems="center"
+          backgroundColor="surface"
+          borderColor="onSurface"
+          borderRadius={16}
+          borderWidth={2}
+          margin="large"
+          paddingTop="largest"
+          width="100%">
+          <Typography fontSize="larger" textTransform="uppercase">
+            quit game?
+          </Typography>
+          <Container
+            flexDirection="row"
+            justifyContent="space-between"
+            marginTop="large"
+            width="100%">
+            <Pressable
+              disabled={disabled}
+              onPress={onPressYes}
+              style={styles.button}>
+              <Typography fontSize="largest">yes</Typography>
+            </Pressable>
+            <Pressable
+              disabled={disabled}
+              onPress={onPressNo}
+              style={styles.button}>
+              <Typography fontSize="largest">no</Typography>
+            </Pressable>
+          </Container>
+        </Container>
+      </Container>
+    </Container>
+  );
+};
+
+const quitGameModalStyles = (theme: Theming.Theme) =>
+  StyleSheet.create<{background: ViewStyle; button: ViewStyle}>({
+    background: {
+      backgroundColor: theme.color.blackTransparent,
+      height: '100%',
+      position: 'absolute',
+      width: '100%',
+    },
+    button: {
+      paddingBottom: theme.spacing.largest,
+      paddingHorizontal: theme.spacing.large,
+      paddingTop: theme.spacing.normal,
+    },
+  });
 
 export default QuitGameModal;
