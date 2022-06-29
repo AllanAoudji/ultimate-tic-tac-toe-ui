a08c2420ff0f64e8ac7c812a6e669a062618707e
AZIHubert <allan.aoudji@protonmail.com>
2022-06-29 20:21:34 +0200

    Store theme.id in local storage when theme is change

--- src/Theme.context.tsx
+++ src/Theme.context.tsx
@@ -1,3 +1,4 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import React from 'react';
 
 import {DEFAULT_DARK_THEME, DEFAULT_DARK_THEME_ID} from './DefaultDark.theme';
@@ -10,37 +11,60 @@ interface Props {
   initialTheme: Theming.Theme;
 }
 interface ProvidedValue {
+  localThemeIsSet: boolean;
   theme: Theming.Theme;
   toggleTheme: () => void;
 }
 
 const ThemeContext = React.createContext<ProvidedValue>({
+  localThemeIsSet: true,
   theme: DEFAULT_LIGHT_THEME,
   toggleTheme: () => {},
 });
 
 const ThemeProvider: React.FC<Props> = ({children, initialTheme}) => {
+  const [localThemeIsSet, setLocalThemeIsSet] = React.useState<boolean>(true);
   const [theme, setTheme] = React.useState<Theming.Theme>(initialTheme);
 
+  let [isMount, setIsMount] = React.useState<boolean>(true);
+
   const toggleTheme = React.useCallback(() => {
+    setIsMount(false);
     setTheme(currentTheme => {
       if (currentTheme.id === DEFAULT_LIGHT_THEME_ID) {
         return DEFAULT_DARK_THEME;
       }
-      if (currentTheme.id === DEFAULT_DARK_THEME_ID) {
-        return DEFAULT_LIGHT_THEME;
-      }
-      return currentTheme;
+      return DEFAULT_LIGHT_THEME;
     });
   }, []);
 
   const value = React.useMemo(
     () => ({
+      localThemeIsSet,
       theme,
       toggleTheme,
     }),
-    [theme, toggleTheme],
+    [localThemeIsSet, theme, toggleTheme],
   );
+
+  React.useEffect(() => {
+    const setLocalTheme = async () => {
+      setLocalThemeIsSet(false);
+      try {
+        if (theme.id === DEFAULT_LIGHT_THEME_ID) {
+          await AsyncStorage.setItem('THEME_ID', DEFAULT_LIGHT_THEME_ID);
+        } else {
+          await AsyncStorage.setItem('THEME_ID', DEFAULT_DARK_THEME_ID);
+        }
+      } finally {
+        setLocalThemeIsSet(true);
+      }
+    };
+    if (!isMount) {
+      setLocalTheme();
+    }
+  }, [isMount, theme]);
+
   return (
     <ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>
   );
