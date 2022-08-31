64c2030d2349885d030d9793262be377d8f877ef
AZIHubert <45357293+AZIHubert@users.noreply.github.com>
2022-08-31 17:56:42 +0200

    create and test <Asset />

--- __tests__/Asset.test.tsx
+++ __tests__/Asset.test.tsx
@@ -1,19 +1,54 @@
 import {render} from '@testing-library/react-native';
 import React from 'react';
 
-import {jsonSource} from './testUtils';
+import {jsonSource, getStyle} from './testUtils';
 
 import Asset from '../src/Asset';
+import {DEFAULT_LIGHT_THEME} from '../src/DefaultLight.theme';
 
 const mockLottie = jest.fn();
-jest.mock('lottie-react-native', () => (props: any) => {
-  const {View} = require('react-native');
-  mockLottie(props);
-  return <View {...props} />;
+const mockPlay = jest.fn();
+jest.mock('lottie-react-native', () => {
+  const {forwardRef, useEffect} = require('react');
+  return forwardRef((props: any, ref: any) => {
+    useEffect(() => {
+      if (ref.current) {
+        ref.current.play = mockPlay;
+      }
+    }, [ref]);
+    const {View} = require('react-native');
+    mockLottie(props);
+    return <View {...props} ref={ref} />;
+  });
 });
 
-const renderer = () => {
-  const renderAsset = render(<Asset />);
+// should have a padding
+// different sizes
+
+const renderer = (
+  options: {
+    disabled?: boolean;
+    padding?: keyof Theming.SpacingTheme;
+    state?: 'EMPTY' | 'PLAY' | 'VISIBLE';
+    type?:
+      | 'LBottomBlue'
+      | 'LBottomRed'
+      | 'LMiddleHorBlue'
+      | 'LMiddleHorRed'
+      | 'LTopBlue'
+      | 'LTopRed'
+      | 'O1'
+      | 'X1';
+  } = {},
+) => {
+  const renderAsset = render(
+    <Asset
+      disabled={options.disabled}
+      padding={options.padding}
+      state={options.state}
+      type={options.type}
+    />,
+  );
 
   const {getByTestId} = renderAsset;
 
@@ -29,11 +64,19 @@ const renderer = () => {
   };
 };
 
+const LBottomBlue = require(jsonSource('LBottomBlue'));
+const LBottomRed = require(jsonSource('LBottomRed'));
+const LMiddleHorBlue = require(jsonSource('LMiddleHorBlue'));
+const LMiddleHorRed = require(jsonSource('LMiddleHorRed'));
+const LTopBlue = require(jsonSource('LTopBlue'));
+const LTopRed = require(jsonSource('LTopRed'));
+const O1 = require(jsonSource('O1'));
 const X1 = require(jsonSource('X1'));
 
 describe('<Asset />', () => {
   beforeEach(() => {
     mockLottie.mockClear();
+    mockPlay.mockClear();
   });
 
   it('renders a <Lottie /> component', () => {
@@ -48,5 +91,151 @@ describe('<Asset />', () => {
         source: X1,
       }),
     );
+  });
+
+  it('renders with /loop === false/', () => {
+    renderer();
+    expect(mockLottie).toHaveBeenCalledWith(
+      expect.objectContaining({
+        loop: false,
+      }),
+    );
+  });
+
+  it('displays the image by default', () => {
+    renderer();
+    expect(mockPlay).toHaveBeenCalledWith(26, 26);
+  });
+
+  it('displays the image by default if /type === "O1"/', () => {
+    renderer({type: 'O1'});
+    expect(mockPlay).toHaveBeenCalledWith(28, 28);
+  });
+
+  it('triggers the animation if /state === "PLAY"/', () => {
+    renderer({state: 'PLAY'});
+    expect(mockPlay).toHaveBeenCalledWith(0, 26);
+  });
+
+  it('triggers the animation if /state === "PLAY"/ and /type === "O1"/', () => {
+    renderer({state: 'PLAY', type: 'O1'});
+    expect(mockPlay).toHaveBeenCalledWith(0, 28);
+  });
+
+  it('displays nothing if /state === "EMPTY"', () => {
+    renderer({state: 'EMPTY'});
+    expect(mockPlay).toHaveBeenCalledWith(0, 0);
+  });
+
+  it('displays with /padding === theme.spacing.smallest/ by default', () => {
+    const {container} = renderer();
+    expect(getStyle(container.get.container())).toEqual(
+      expect.objectContaining({
+        padding: undefined,
+      }),
+    );
+  });
+
+  it('displays with another /padding/', () => {
+    const {container} = renderer({padding: 'large'});
+    expect(getStyle(container.get.container())).toEqual(
+      expect.objectContaining({
+        padding: DEFAULT_LIGHT_THEME.spacing.large,
+      }),
+    );
+  });
+
+  describe('renders with /source === ', () => {
+    it('LBottomBlue/', () => {
+      renderer({type: 'LBottomBlue'});
+      expect(mockLottie).toHaveBeenCalledWith(
+        expect.objectContaining({
+          source: LBottomBlue,
+        }),
+      );
+    });
+
+    it('LBottomRed/', () => {
+      renderer({type: 'LBottomRed'});
+      expect(mockLottie).toHaveBeenCalledWith(
+        expect.objectContaining({
+          source: LBottomRed,
+        }),
+      );
+    });
+
+    it('LMiddleHorBlue/', () => {
+      renderer({type: 'LMiddleHorBlue'});
+      expect(mockLottie).toHaveBeenCalledWith(
+        expect.objectContaining({
+          source: LMiddleHorBlue,
+        }),
+      );
+    });
+
+    it('LMiddleHorRed/', () => {
+      renderer({type: 'LMiddleHorRed'});
+      expect(mockLottie).toHaveBeenCalledWith(
+        expect.objectContaining({
+          source: LMiddleHorRed,
+        }),
+      );
+    });
+
+    it('LTopBlue/', () => {
+      renderer({type: 'LTopBlue'});
+      expect(mockLottie).toHaveBeenCalledWith(
+        expect.objectContaining({
+          source: LTopBlue,
+        }),
+      );
+    });
+
+    it('LTopRed/', () => {
+      renderer({type: 'LTopRed'});
+      expect(mockLottie).toHaveBeenCalledWith(
+        expect.objectContaining({
+          source: LTopRed,
+        }),
+      );
+    });
+
+    it('O1/', () => {
+      renderer({type: 'O1'});
+      expect(mockLottie).toHaveBeenCalledWith(
+        expect.objectContaining({
+          source: O1,
+        }),
+      );
+    });
+
+    it('X1/', () => {
+      renderer({type: 'X1'});
+      expect(mockLottie).toHaveBeenCalledWith(
+        expect.objectContaining({
+          source: X1,
+        }),
+      );
+    });
+  });
+
+  describe('displays with /optacity ===', () => {
+    it('1/ by default', () => {
+      const {container} = renderer();
+      expect(getStyle(container.get.container())).toEqual(
+        expect.objectContaining({
+          opacity: 1,
+        }),
+      );
+    });
+
+    it('0.4/ if /disabled === true/', () => {
+      const {container} = renderer({disabled: true});
+      expect(getStyle(container.get.container())).toEqual(
+        expect.objectContaining({
+          opacity: 0.4,
+        }),
+      );
+    });
   });
 });
--- src/Asset.tsx
+++ src/Asset.tsx
@@ -1,30 +1,174 @@
 import React from 'react';
 import Lottie from 'lottie-react-native';
+import {StyleSheet, ViewStyle} from 'react-native';
+import {ThemeContext} from './Theme.context';
 
 type props = {
+  disabled?: boolean;
+  padding?: keyof Theming.SpacingTheme;
   state?: 'EMPTY' | 'PLAY' | 'VISIBLE';
+  type?:
+    | 'LBottomBlue'
+    | 'LBottomRed'
+    | 'LMiddleHorBlue'
+    | 'LMiddleHorRed'
+    | 'LTopBlue'
+    | 'LTopRed'
+    | 'O1'
+    | 'X1';
 };
 
-const Asset: React.FC<props> = ({state}) => {
+// Each animation have different nums of frames
+// this object is used in animationRef.current?play()
+// to know how many frames should be used to trigger the animation
+const ANIMATION_LAST_FRAME = {
+  LBottomBlue: 28,
+  LBottomRed: 28,
+  LMiddleHorBlue: 28,
+  LMiddleHorRed: 28,
+  LTopBlue: 28,
+  LTopRed: 28,
+  O1: 28,
+  X1: 26,
+};
+
+const Asset: React.FC<props> = ({
+  disabled = false,
+  padding,
+  state,
+  type = 'X1',
+}) => {
   const animationRef = React.useRef<Lottie>(null);
+
+  const {theme} = React.useContext(ThemeContext);
+  const stylesProps = React.useMemo(
+    () => stylesAsset({disabled, padding}),
+    [disabled, padding],
+  );
+  const styles = React.useMemo(() => stylesProps(theme), [stylesProps, theme]);
 
   React.useEffect(() => {
     if (state === 'EMPTY') {
       animationRef.current?.play(0, 0);
     } else if (state === 'PLAY') {
-      animationRef.current?.play(0, 26);
+      animationRef.current?.play(0, ANIMATION_LAST_FRAME[type]);
     } else {
-      animationRef.current?.play(26, 26);
+      animationRef.current?.play(
+        ANIMATION_LAST_FRAME[type],
+        ANIMATION_LAST_FRAME[type],
+      );
     }
-  }, [state]);
+  }, [state, type]);
+
+  if (type === 'LBottomBlue') {
+    return (
+      <Lottie
+        loop={false}
+        ref={animationRef}
+        source={require('../assets/jsons/LBottomBlue.json')}
+        style={styles.container}
+        testID="asset__container--lottie"
+      />
+    );
+  }
+
+  if (type === 'LBottomRed') {
+    return (
+      <Lottie
+        loop={false}
+        ref={animationRef}
+        source={require('../assets/jsons/LBottomRed.json')}
+        style={styles.container}
+        testID="asset__container--lottie"
+      />
+    );
+  }
+
+  if (type === 'LMiddleHorBlue') {
+    return (
+      <Lottie
+        loop={false}
+        ref={animationRef}
+        source={require('../assets/jsons/LMiddleHorBlue.json')}
+        style={styles.container}
+        testID="asset__container--lottie"
+      />
+    );
+  }
+
+  if (type === 'LMiddleHorRed') {
+    return (
+      <Lottie
+        loop={false}
+        ref={animationRef}
+        source={require('../assets/jsons/LMiddleHorRed.json')}
+        testID="asset__container--lottie"
+        style={styles.container}
+      />
+    );
+  }
+
+  if (type === 'LTopBlue') {
+    return (
+      <Lottie
+        loop={false}
+        ref={animationRef}
+        source={require('../assets/jsons/LTopBlue.json')}
+        style={styles.container}
+        testID="asset__container--lottie"
+      />
+    );
+  }
+
+  if (type === 'LTopRed') {
+    return (
+      <Lottie
+        loop={false}
+        ref={animationRef}
+        source={require('../assets/jsons/LTopRed.json')}
+        style={styles.container}
+        testID="asset__container--lottie"
+      />
+    );
+  }
+
+  if (type === 'O1') {
+    return (
+      <Lottie
+        loop={false}
+        ref={animationRef}
+        source={require('../assets/jsons/O1.json')}
+        style={styles.container}
+        testID="asset__container--lottie"
+      />
+    );
+  }
 
   return (
     <Lottie
-      source={require('../assets/jsons/X1.json')}
+      loop={false}
       ref={animationRef}
+      source={require('../assets/jsons/X1.json')}
       testID="asset__container--lottie"
+      style={styles.container}
     />
   );
 };
+
+const stylesAsset =
+  ({
+    disabled,
+    padding,
+  }: {
+    disabled: boolean;
+    padding?: keyof Theming.SpacingTheme;
+  }) =>
+  (theme: Theming.Theme) =>
+    StyleSheet.create<{container: ViewStyle}>({
+      container: {
+        opacity: disabled ? 0.4 : 1,
+        padding: padding ? theme.spacing[padding] : undefined,
+      },
+    });
 
 export default Asset;
