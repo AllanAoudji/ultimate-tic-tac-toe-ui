b58467b9f1ab7fb1eab13c92bb895ba0812af722
AZIHubert <45357293+AZIHubert@users.noreply.github.com>
2022-09-06 19:07:38 +0200

    adds a visible props to <WinningModalWrapper /> to display the modal

--- __tests__/WinningModalWrapper.test.tsx
+++ __tests__/WinningModalWrapper.test.tsx
@@ -1,68 +1,121 @@
 import {fireEvent, render} from '@testing-library/react-native';
 import React from 'react';
+import {GestureResponderEvent} from 'react-native';
 import {TileState} from 'ultimate-tic-tac-toe-algorithm';
 
 import WinningModalWrapper from '../src/WinningModalWrapper';
 
+const renderer = (
+  options: {
+    disabled?: boolean;
+    onPressQuit?: ((event: GestureResponderEvent) => void) | null | undefined;
+    onPressNewGame?:
+      | ((event: GestureResponderEvent) => void)
+      | null
+      | undefined;
+    visible?: boolean;
+    winner?: TileState;
+  } = {},
+) => {
+  const renderWinningModalWrapper = render(
+    <WinningModalWrapper
+      disabled={options.disabled}
+      onPressQuit={options.onPressQuit}
+      onPressNewGame={options.onPressNewGame}
+      visible={options.visible}
+      winner={options.winner}
+    />,
+  );
+
+  const QUIT_TEXT = 'quit';
+  const NEW_GAME_TEXT = 'new game';
+  const WINNING_MODAL_CONTAINER_TEST_ID = 'winningModal__container';
+
+  const {getByTestId, getByText, queryByTestId, queryByText} =
+    renderWinningModalWrapper;
+
+  const getContainer = () => getByTestId(WINNING_MODAL_CONTAINER_TEST_ID);
+  const getNewGameButton = () => getByText(NEW_GAME_TEXT);
+  const getQuitButton = () => getByText(QUIT_TEXT);
+
+  const queryContainer = () => queryByTestId(WINNING_MODAL_CONTAINER_TEST_ID);
+  const queryNewGameButton = () => queryByText(NEW_GAME_TEXT);
+  const queryQuitButton = () => queryByText(QUIT_TEXT);
+
+  return {
+    container: {
+      get: {
+        container: getContainer,
+        newGameButton: getNewGameButton,
+        quitButton: getQuitButton,
+      },
+      press: {
+        newGameButton: () => {
+          fireEvent.press(getNewGameButton());
+        },
+        quitButton: () => {
+          fireEvent.press(getQuitButton());
+        },
+      },
+      query: {
+        container: queryContainer,
+        newGameButton: queryNewGameButton,
+        quitButton: queryQuitButton,
+      },
+    },
+    renderWinningModalWrapper,
+  };
+};
+
 describe('<WinningModalWrapper />', () => {
-  const PLAYER_X_TEXT = 'player x',
-    NEW_GAME_TEXT = 'new game',
-    QUIT_TEXT = 'quit',
-    WINNING_MODAL_CONTAINER_TEST_ID = 'winningModal__container';
-  let onPress: jest.Mock;
-
-  beforeEach(() => {
-    onPress = jest.fn();
-  });
-
-  afterEach(() => {
-    onPress.mockRestore();
-  });
-
   it('do not renders <WinningModal /> if /winner === Empty/', () => {
-    const {queryByTestId} = render(
-      <WinningModalWrapper winner={TileState.Empty} />,
-    );
-    expect(queryByTestId(WINNING_MODAL_CONTAINER_TEST_ID)).toBeNull();
+    const {container} = renderer();
+    expect(container.query.container()).toBeNull();
   });
 
-  it('renders <WinningModal /> if /winner === Player/', () => {
-    const {queryByText} = render(
-      <WinningModalWrapper winner={TileState.Player1} />,
-    );
-    expect(queryByText(PLAYER_X_TEXT)).not.toBeNull();
+  it('do not renders <WinningModal /> if /visible === false/', () => {
+    const {container} = renderer({winner: TileState.Player1});
+    expect(container.query.container()).toBeNull();
+  });
+
+  it('renders <WinningModal /> if /winner === Player/ and /visible === true/', () => {
+    const {container} = renderer({visible: true, winner: TileState.Player1});
+    expect(container.get.container()).not.toBeNull();
   });
 
   it('passes /onPressNewGame/ to <WinningModal />', () => {
-    const {getByText} = render(
-      <WinningModalWrapper
-        onPressNewGame={onPress}
-        winner={TileState.Player1}
-      />,
-    );
-    fireEvent.press(getByText(NEW_GAME_TEXT));
+    const onPress = jest.fn();
+    const {container} = renderer({
+      onPressNewGame: onPress,
+      visible: true,
+      winner: TileState.Player1,
+    });
+    container.press.newGameButton();
     expect(onPress).toHaveBeenCalled();
   });
 
   it('passes /onPressQuit/ to <WinningModal />', () => {
-    const {getByText} = render(
-      <WinningModalWrapper onPressQuit={onPress} winner={TileState.Player1} />,
-    );
-    fireEvent.press(getByText(QUIT_TEXT));
+    const onPress = jest.fn();
+    const {container} = renderer({
+      onPressQuit: onPress,
+      visible: true,
+      winner: TileState.Player1,
+    });
+    container.press.quitButton();
     expect(onPress).toHaveBeenCalled();
   });
 
   it('passes /disabled/ to <WinningModal />', () => {
-    const {getByText} = render(
-      <WinningModalWrapper
-        disabled={true}
-        onPressNewGame={onPress}
-        onPressQuit={onPress}
-        winner={TileState.Player1}
-      />,
-    );
-    fireEvent.press(getByText(QUIT_TEXT));
-    fireEvent.press(getByText(NEW_GAME_TEXT));
+    const onPress = jest.fn();
+    const {container} = renderer({
+      disabled: true,
+      onPressNewGame: onPress,
+      onPressQuit: onPress,
+      visible: true,
+      winner: TileState.Player1,
+    });
+    container.press.newGameButton();
+    container.press.quitButton();
     expect(onPress).not.toHaveBeenCalled();
   });
 });
